a18595accbffaddad7e05e0a6a1c0103fd286bd0
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2022-02-21 15:46:14 +0100

    add fake data

--- database/seeds/trainsTableSeeder.php
+++ database/seeds/trainsTableSeeder.php
@@ -17,14 +17,14 @@ class trainsTableSeeder extends Seeder
         for ($i=0; $i < 10 ; $i++) { 
             $newTrain = new Trains();
             $newTrain -> Azienda = $faker -> words(4, true);
-            $newTrains -> Stazione_di_partenza = $faker -> words(7,true);
-            $newTrains -> Stazione_di_arrivo = $faker -> words(7, true);
-            $newTrains -> Orario_di_partenza = $faker -> time();
-            $newTrains -> Orario_di_arrivo = $faker -> time();
+            $newTrain -> Stazione_di_partenza = $faker -> words(7,true);
+            $newTrain -> Stazione_di_arrivo = $faker -> words(7, true);
+            $newTrain -> Orario_di_partenza = $faker -> time();
+            $newTrain -> Orario_di_arrivo = $faker -> time();
             $newTrain -> Codice_treno = $faker -> uuid();
             $newTrain -> Numero_carrozze = $faker -> randomDigit();
-            $newTrain -> In_orario = $faker -> numberBetween();
-            $newTrain -> Cancellato = $faker -> numberBetween();
+            $newTrain -> In_orario = $faker -> numberBetween(0,1);
+            $newTrain -> Cancellato = $faker -> numberBetween(0,1);
             $newTrain -> save();
         }
     }
